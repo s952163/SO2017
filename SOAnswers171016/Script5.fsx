5c9d82549578ce628fbb2333514d4d0e00bf81d2
s952163 <s995110@yahoo.co.jp>
2017-11-14 18:44:38 +0900

    SO171114 jsonvalue array

--- SOAnswers171016/Script5.fsx
+++ SOAnswers171016/Script5.fsx
@@ -1,4 +1,8 @@
 #r @"../packages/FSharp.Data/lib/net40/FSharp.Data.dll"
+open FSharp.Data
+open FSharp.Data
+open FSharp.Data
+open FSharp.Data
 
 open FSharp.Data
 open FSharp.Data.JsonExtensions
@@ -11,7 +15,12 @@ let jsonText = """
         "Id": "001",
         "Name": "A. N. Other",
         "IdDatetime": "2017-11-11T13:10:00+00:00"
-        }
+        },
+    "236": {
+        "Id": "002",
+        "Name": "X. Y. Other",
+        "IdDatetime": "2016-11-11T13:10:00+00:00"
+    }
     }
 }
 """
@@ -27,3 +36,48 @@ let json4 =
     match json3 with
     [|(_,x)|] -> x
 
+[<Literal>]
+let jsonText2 = """
+{
+"record": {
+    "235": {
+        "Id": "001",
+        "Name": "A. N. Other",
+        "IdDatetime": "2017-11-11T13:10:00+00:00"
+    },
+    "255": {
+        "Id": "005",
+        "Name": "D. Other",
+        "IdDatetime": "2017-11-11T13:10:00+00:00"
+    }
+  }
+}
+"""
+
+let json1 = JsonValue.Parse(jsonText2)
+let json2 = json1?record
+
+let json3 = 
+    match json2 with
+        | JsonValue.Record x -> x
+
+
+let json4 = 
+json3 
+|> Array.map (function _,x -> x) 
+
+let json4 = 
+    match json3 with
+    [|(_,x)|] -> x
+
+
+
+let extractInnerJson (x: (string * JsonValue) []) =
+    match x with  
+    [|(_,x)|] -> Some x
+    | _ -> None
+
+//let (json4:JsonValue []) =
+let json4 = 
+    json3
+    |> extractInnerJson
