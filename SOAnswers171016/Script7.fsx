649464e82bb07afaf7556e4625055a0286ba0b74
s952163 <s995110@yahoo.co.jp>
2017-12-19 19:18:28 +0900

    SO171219

--- SOAnswers171016/Script7.fsx
+++ SOAnswers171016/Script7.fsx
@@ -8,3 +8,7 @@ ms |> dict |> Seq.map (fun (KeyValue (k, v)) -> v)
 
 
 seq { 1.. 4} |> Seq.replicate 10000 |> Seq.concat |> Seq.take 10 |> Seq.toArray
+
+seq { 1 .. 4} |> Seq.toArray |> Array.take 7
+
+Seq.initInfinite (fun _ -> seq { 1 .. 4 } ) |> Seq.concat |> Seq.take 5 |> Seq.toArray 
