e1664761b5a6d9f33281aca8557d75a2978fac39
s952163 <s995110@yahoo.co.jp>
2017-12-19 18:35:58 +0900

    171219

--- SOAnswers171016/Script7.fsx
+++ SOAnswers171016/Script7.fsx
@@ -5,3 +5,6 @@ let map1 = ms |> Map.ofList
 map1 |> Map.toArray |> Array.map snd
 
 ms |> dict |> Seq.map (fun (KeyValue (k, v)) -> v)
+
+
+seq { 1.. 4} |> Seq.replicate 10000 |> Seq.concat |> Seq.take 10 |> Seq.toArray
