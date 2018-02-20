af3ecd79f84acceec0d5e43d63dd01f19441a075
s952163 <s995110@yahoo.co.jp>
2018-02-20 15:54:53 +0900

    .

--- SO180207/Script.fsx
+++ SO180207/Script.fsx
@@ -1,5 +1,24 @@
 #load "TestModule.fs"
+open System
 
 open TestModule
 
 MyModule.testFunc
+
+
+let inline f t = (^T : (member Id:int) t)
+
+type MyRec1 = {
+    Id: int
+    Name: string 
+    }
+
+type MyClass(id: int, address: string) = 
+    member __.Id = id
+    member __.Address = address 
+
+let myrec1 = {Id=100; Name= "jim"}
+let myclass = MyClass(id=99, address = "NYC")
+
+f myrec1
+f myclass 
