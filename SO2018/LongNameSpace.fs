c98576396535a46346812a3f360e9e1a47ad6c8b
s952163 <s995110@yahoo.co.jp>
2018-02-22 18:38:19 +0900

    Crated S02018 project 180222

---
+++ SO2018/LongNameSpace.fs
@@ -0,0 +1,7 @@
+module ThisIs.AveryLong.NameSpace
+
+type MyClass() =
+     static member X = 8
+
+     
+
---
+++ SO2018/SO2018.fs
@@ -0,0 +1,9 @@
+module SO2018
+
+module Short = ThisIs.AveryLong.NameSpace
+//type Short =  ThisIs.AveryLong.NameSpace
+
+[<EntryPoint>]
+let main argv =
+    printfn "%A" argv
+    0 // return an integer exit code
