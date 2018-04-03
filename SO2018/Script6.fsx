ce8e314762022531be8b0a1b08ce437991690026
s952163 <s995110@yahoo.co.jp>
2018-04-03 20:12:21 +0900

    .

--- SO2018/Script6.fsx
+++ SO2018/Script6.fsx
@@ -1,8 +1,16 @@
+// type CapturablePieces = Pawn | Knight | Bishop | Rook | Queen
+
+// //type Pieces = CapturablePieces of Pawn | Knight | Bishop | Rook | Queen | King
+// type Pieces = Capturable of CapturablePieces | King
+// let x = Pawn
+// let y = Capturable(Pawn)
+
+// let z = King
+
+
 type CapturablePieces = Pawn | Knight | Bishop | Rook | Queen
 
-//type Pieces = CapturablePieces of Pawn | Knight | Bishop | Rook | Queen | King
-type Pieces = Capturable of CapturablePieces | King
-let x = Pawn
-let y = Capturable(Pawn)
+type Pieces =
+    | Capturables of CapturablePieces
+    | King
 
-x
