76bab26f87518a971648ede91216acba5aafb251
s952163 <s995110@yahoo.co.jp>
2018-04-10 12:14:46 +0900

    .

--- SO2018/Script7.fsx
+++ SO2018/Script7.fsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let hold y z median c1 c2 =            
         if  ((y > median) && (z > median)) then (c1,c1)
         elif ((y <= median) && (z <= median)) then (c2,c2)
@@ -17,32 +16,3 @@ xs
                           if x > 10 then (x , x)
                           elif y >= 10 then (y , y)
                           else (x,y) )
-=======
-// type CapturablePieces = Pawn | Knight | Bishop | Rook | Queen
-
-// //type Pieces = CapturablePieces of Pawn | Knight | Bishop | Rook | Queen | King
-// type Pieces = Capturable of CapturablePieces | King
-// let x = Pawn
-// let y = Capturable(Pawn)
-
-// let z = King
-
-
-type CapturablePieces = Pawn | Knight | Bishop | Rook | Queen
-
-type Pieces =
-    | Capturables of CapturablePieces
-    | King
-
-let z = Capturables Pawn
-
-let (Capturables xx ) = z
-
-let x1 = 
-    async {
-        let y = 2
-        let z = 3
-        let x = async { return (y + z) } 
-        return! x
-    } |> Async.RunSynchronously
->>>>>>> b68161ec0f7a46e0206eefa9c68aa191339d0b20
