29ac62038188731d48f02d1163d58f7060b7f920
s952163 <s995110@yahoo.co.jp>
2018-05-11 08:45:21 +0900

    .

--- SO2018/Script11.fsx
+++ SO2018/Script11.fsx
@@ -1,17 +1,23 @@
+ open System
  
  type ModelArg = {
      Number: int
      Name: string
  }
  
+ [<AbstractClass>]
  type IModel(modelArg: ModelArg) = 
     abstract member Number: int
     abstract member Name: string
+    abstract member NameAndNum: string -> string
     default __.Number = modelArg.Number
     default __.Name = modelArg.Name
 
+
 type ConcreteModel1(modelArg: ModelArg) =
     inherit IModel(modelArg)
+    override __.Number = 42
+    override __.NameAndNum(x: string) = "Hello" + x
 
 type ConcreteModel2(modelArg: ModelArg) =
     inherit IModel(modelArg)
@@ -23,10 +29,21 @@ let getNumberAndName(x: IModel) =
     (x.Number, x.Name)
 
 let model1 = ConcreteModel1(modelArg1)
-let model2 = ConcreteModel2(modelArg2)
+let model2 = ConcreteModel2(modelArg2);;
 
-getNumberAndName(model1)
+getNumberAndName(model1);;
 //val it : int * string = (2, "Joe")
 getNumberAndName(model2)
 //val it : int * string = (3, "Jim")
 
+type BaseClass(modelArg: ModelArg) =
+    member __.Number = modelArg.Number
+    member __.Name = modelArg.Name
+
+let baseclass =  BaseClass(modelArg1)
+
+type SubClass(input: BaseClass) =
+    member __.X = input
+
+let subclass = SubClass(baseclass)
+subclass.X.Name
