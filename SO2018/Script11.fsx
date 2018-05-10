7aafa743406cbe8b54a14f328e2a2b98e018f723
s952163 <s995110@yahoo.co.jp>
2018-05-10 09:37:46 +0900

    SO180510

--- SO2018/Script11.fsx
+++ SO2018/Script11.fsx
@@ -1,1 +1,32 @@
  
+ type ModelArg = {
+     Number: int
+     Name: string
+ }
+ 
+ type IModel(modelArg: ModelArg) = 
+    abstract member Number: int
+    abstract member Name: string
+    default __.Number = modelArg.Number
+    default __.Name = modelArg.Name
+
+type ConcreteModel1(modelArg: ModelArg) =
+    inherit IModel(modelArg)
+
+type ConcreteModel2(modelArg: ModelArg) =
+    inherit IModel(modelArg)
+
+let modelArg1 = {Number=2; Name ="Joe"}
+let modelArg2 = {Number=3; Name = "Jim"}
+
+let getNumberAndName(x: IModel) =
+    (x.Number, x.Name)
+
+let model1 = ConcreteModel1(modelArg1)
+let model2 = ConcreteModel2(modelArg2)
+
+getNumberAndName(model1)
+//val it : int * string = (2, "Joe")
+getNumberAndName(model2)
+//val it : int * string = (3, "Jim")
+
