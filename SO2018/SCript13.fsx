3da870925c4bfd8a0e5d2f0bc95d42f5dd50216a
s952163 <s995110@yahoo.co.jp>
2018-05-15 14:15:16 +0900

    SO180515

--- SO2018/SCript13.fsx
+++ SO2018/SCript13.fsx
@@ -1,3 +1,19 @@
 
-if true then "Yes" else "No"
+if true then "Yes" else "No";;
 
+match true with
+    | true -> "Yes"
+    | _  -> "false"
+
+
+let myif =
+    function 
+        | true -> "Yes"
+        | _ -> "false"
+
+true |> myif
+
+
+let myif2 x = if x then "Yes" else "No"
+
+true |> myif2
